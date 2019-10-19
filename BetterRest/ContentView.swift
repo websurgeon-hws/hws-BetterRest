448d6ea234f1b8da438ff06f39d34b624b2f2c6d
Peter Barclay <peter@peter-barclay.com>
2019-10-19 09:19:43 +0100

    add step value

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,7 +8,7 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     
     var body: some View {
-        Stepper(value: $sleepAmount, in: 4...12) {
+        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
             Text("\(self.sleepAmount) hours")
         }
     }
