f4cf31abda9d76b678c48fedae7321e57552ac51
Peter Barclay <peter@peter-barclay.com>
2019-10-19 09:26:40 +0100

    labelsHidden modifier

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,9 +8,8 @@ struct ContentView: View {
     @State private var wakeUp = Date()
     
     var body: some View {
-        Form {
-            DatePicker("Please enter a date", selection: $wakeUp)
-        }
+        DatePicker("Please enter a date", selection: $wakeUp)
+        .labelsHidden()
     }
 }
 
