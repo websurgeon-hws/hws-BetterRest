b7985476c616c62091ddd42da47ad31867b88aed
Peter Barclay <peter@peter-barclay.com>
2019-10-19 09:36:22 +0100

    DateComponents and Calendar

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,7 +8,12 @@ struct ContentView: View {
     @State private var wakeUp = Date()
     
     var body: some View {
-        DatePicker("Please enter a date",
+        var components = DateComponents()
+        components.hour = 8
+        components.minute = 0
+        let date = Calendar.current.date(from: components) ?? Date()
+        
+        return DatePicker("Please enter a date",
                    selection: $wakeUp,
                    in: Date()...)
         .labelsHidden()
